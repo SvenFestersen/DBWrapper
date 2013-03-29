c44ea17e7cd3f8adf6add061d140fa44abf42164
Sven Festersen <sven@sven-festersen.de>
2013-03-29 18:13:06 +0100

    Changed project layout and updated setup.py for PyPI.

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 Setup file for DBWrapper.
 
 """
+import os
 from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
       name='DBWrapper',
@@ -13,4 +17,14 @@ setup(
       author_email='sven@sven-festersen.de',
       url='https://github.com/SvenFestersen/DBWrapper',
       packages=['dbwrapper'],
+      license='GPL',
+      keywords='database sqlite threading',
+      long_description=read('README'),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development'
+      ],
      )
